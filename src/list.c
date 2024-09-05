076d24afd9564db975182f6cb515bdb2e4a8815f
Michael Mehr <michaelmehr0@gmail.com>
2024-09-05 17:01:33 -0500

    created list.c with function prototypes, and list.h with a struct for singly-linked lists

---
+++ src/list.c
@@ -0,0 +1,6 @@
+// list c
+
+void list_add(struct list_element *list_head, struct list_element *new_element);
+
+
+void list_remove(struct list_element *element);
---
+++ src/list.h
@@ -0,0 +1,8 @@
+// list.h
+
+struct list_element {
+  struct list_element *next;
+  int data;
+};
+
+
