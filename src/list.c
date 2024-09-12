38c6429962d61177d484e1c03b567ef01042602d
Michael <michaelmehr0@gmail.com>
2024-09-12 17:23:56 -0500

    list.c compiles

--- src/list.c
+++ src/list.c
@@ -5,36 +5,28 @@
 
 
 void list_add(struct list_element *list_head, struct list_element *new_element){
-  *new_element->next = *list_head;
-  *list_head = *new_element;
+  new_element->next = list_head;
+  list_head = new_element;
 }
 
 
-void list_remove(struct list_element *list_head, struct list_element *element_to_remove){
-  struct list_element *current_element = (*list_head);
-  struct list_element *prev_element = NULL;
-
-  while (*current_element->next) {
-    *prev_element = *current_element;
-    *current_element = current_element->next;
-    if (*current_element == *element_to_remove){
-      prev_element->next = current_element->next;
-    }
-  }
+void list_remove(struct list_element *element){
+  element->next = NULL;
 }
 
 
 int main(){
+  struct list_element d = {NULL, 0};
   struct list_element c = {NULL, 0};
   struct list_element b = {&c,   0};
   struct list_element a = {&b,   0};
   struct list_element *head = &a;
 
-  struct list_element new_item = {NULL, 0};
+  struct list_element *new_item = &d;
 
-  list_add(*head, *new_item);
+  list_add(head, new_item);
 
-  list_remove();
+  list_remove(head);
 
   return 0;
 }
