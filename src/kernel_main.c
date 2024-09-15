9cc0c8e4a0c3592bde6e056f1179ae78ba816a63
Michael Mehr <michaelmehr0@gmail.com>
2024-09-15 12:48:25 -0500

    commented out unfinished timer

--- src/kernel_main.c
+++ src/kernel_main.c
@@ -1,10 +1,10 @@
 
 char glbl[1024];
 
-unsigned long get_timer_count() {
-  unsigned long *timer_count_register = 0x3f003004;
-  return *timer_count_register;
-}
+//unsigned long get_timer_count() {
+//  unsigned long *timer_count_register = 0x3f003004;
+//  return *timer_count_register;
+//}
 
 void kernel_main() {
 
